37ec027c79c615772bc990b55639efc6442ee7f6
unknown <mchoudhuryakash@gmail.com>
2025-04-15 20:45:47 +0600

    MiddleWare update

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,8 +1,16 @@
-const mongoose = require("mongoose")
-const bcrypt = require("bcryptjs")
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     email: { type:String, require: true, unique: true },
     password: { type:String, require: true},
     role: { type:String, require: true, enum: ['admin','seller','buyer']},
 });
+
+userSchema.pre('save', async function (next){
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password,10)
+});
+
+const User = mongoose.model(User, userSchema)
+module.exports = User;
